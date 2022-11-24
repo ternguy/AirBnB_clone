c975584b5ba37d98b04af22fba715515fce2829b
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-11-23 23:44:12 -0800

    State.py

---
+++ models/State.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""this module creates a user class"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class for managing state objects"""
+
+    name = ""
