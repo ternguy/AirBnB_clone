4182347d9db5416e6ae4515098e76a9094e42704
KitwanaSh <rodriguekitweze@gmail.com>
2022-11-24 10:32:02 +0200

    Fix: Rearange the code for the file_storage.py file

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This script is the base model"""
 
-import uuid
+from uuid import uuid4
 from datetime import datetime
 import models
 
@@ -18,39 +18,47 @@ class BaseModel:
             - **kwargs: dict of key-values arguments
         """
 
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if "created_at" == key:
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                            date_format)
+                elif "updated_at" == key:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                            date_format)
+                elif "__class__" == key:
+                    pass
                 else:
-                    self.__dict__[key] = kwargs[key]
+                    setattr(self, key, value)
+
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
 
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        return ("[{}] ({}) {}".
+            format(self.__class__.__name__, self.id, self.__dict__))
 
+    def __repr__(self):
+        """ make a representation copy of the __str__ """
+        
+        return (self.__str__())
     def save(self):
         """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
 
         my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
+        my_dict["__class__"] = self.__class__.__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+        return (my_dict)
