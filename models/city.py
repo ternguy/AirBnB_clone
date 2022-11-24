77e214928ac1aa3dbb048983465390fe3a8559ef
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-11-23 23:56:35 -0800

    city.py

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""this module is created from user class"""
+
+from models.base_model import BaseModel
+
+class city(BaseModel):
+    """this class is for managing city objects"""
+    state_id = ""
+    name = ""
