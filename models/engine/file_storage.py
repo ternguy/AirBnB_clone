e03776a6f8e9675d75393377b49874fea45060f7
KitwanaSh <rodriguekitweze@gmail.com>
2022-11-24 14:03:43 +0200

    Feat: Update file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,11 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 class FileStorage():
     """ Create a file storage
@@ -22,7 +27,10 @@ class FileStorage():
 
     __file_path = "file.json"
     __objects = {}
-    class_dict = {"BaseModel": BaseModel, "User": User}
+    class_dict = {"BaseModel": BaseModel, "User": User,
+            "State": State, "City": City,
+            "Place": Place, "Amenity": Amenity,
+            "Review": Review}
     
     def all(self):
         """ return the dictionary objects """
