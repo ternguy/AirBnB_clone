c71642267632517181d46faff4538686c5f64dc1
KitwanaSh <rodriguekitweze@gmail.com>
2022-11-25 09:57:53 +0200

    Del: Remove the city

--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""this module is created from user class"""
-from models.base_model import BaseModel
-
-class City(BaseModel):
-    """ City id and name """
-
-    state_id = ""
-    name = ""
