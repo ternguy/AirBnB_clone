50f144b3aac083c9a35417d0ed5dc61a51995afa
KitwanaSh <rodriguekitweze@gmail.com>
2022-11-25 10:00:49 +0200

    Feat: Add back the city console

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Sub_class City of class BaseModel
+"""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """ the city name and id """
+
+    state_id = ""
+    name = ""
