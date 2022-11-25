7b48f5a8b331c36d80b57179ae169c424323d970
KitwanaSh <rodriguekitweze@gmail.com>
2022-11-25 09:51:35 +0200

    Fix: Add back the sub classes

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Sub_class of the BaseModel
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """ Declare the name attribute """
+
+    name = """
---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Sub_class of the Base_model mother class
+"""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """ Show some review attributes """
+
+    place_id = ""
+    user_id
+    text = ""
